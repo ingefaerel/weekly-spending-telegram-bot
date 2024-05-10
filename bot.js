8e7472ffc510a9942cae29e5d97db46a1ba46d91
ingefaerel <elmiramust2010@gmail.com>
2024-05-10 23:26:10 +0200

    added CZK and Left values

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ bot.on('message', (msg) => {
         bot.sendMessage(chatId, 'Welcome to the spending tracker bot!');
     } else if (messageText.startsWith('/setlimit')) {
         weeklyLimit = parseFloat(messageText.split(' ')[1]);
-        bot.sendMessage(chatId, `Weekly limit set to ${weeklyLimit}.`);
+        bot.sendMessage(chatId, `Weekly limit set to ${weeklyLimit} CZK.`);
     } else if (messageText === '/startweek') {
         startDate = new Date();
         totalSpent = 0;
@@ -24,17 +24,17 @@ bot.on('message', (msg) => {
     } else if (messageText.startsWith('/spend')) {
         const amountSpent = parseFloat(messageText.split(' ')[1]);
         totalSpent += amountSpent;
-        bot.sendMessage(chatId, `You spent ${amountSpent}.`);
+        bot.sendMessage(chatId, `You spent ${amountSpent} CZK.`);
         if (totalSpent > weeklyLimit) {
             const difference = totalSpent - weeklyLimit;
             weeklyLimit -= difference;
             bot.sendMessage(chatId, `Weekly limit exceeded! Adjusted to ${weeklyLimit}.`);
         }
     } else if (messageText === '/getstatus') {
-        let statusMsg = `Weekly Limit: ${weeklyLimit}\n`;
-        statusMsg += `Total Spent: ${totalSpent}\n`;
-        statusMsg += `Week Start Date: ${startDate}\n`;
-        statusMsg += `Left: ${weeklyLimit - totalSpent}`;
+        let statusMsg = `Weekly Limit: ${weeklyLimit} CZK\n`;
+        statusMsg += `Total Spent: ${totalSpent} CZK\n`;
+        statusMsg += `Week Start Date: ${startDate} CZK\n`;
+        statusMsg += `Left: ${weeklyLimit - totalSpent} CZK`;
         bot.sendMessage(chatId, statusMsg);
     } else if (messageText === '/help') {
         const helpMsg = `Commands:\n/setlimit [amount] - Set weekly spending limit\n/startweek - Start a new week\n/spend [amount] - Log spending\n/getstatus - Get current status\n/help - Display this help message`;
