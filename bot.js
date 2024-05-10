32dd903fc3231241fd317d1708488fdabac75ea1
ingefaerel <elmiramust2010@gmail.com>
2024-05-10 23:30:35 +0200

    added commands at the start

--- bot.js
+++ bot.js
@@ -3,6 +3,8 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const token = '6807636962:AAFnNvr0iyCG80Ta1evpNzx3yKBvXnAmqSw'; // Replace with your own bot token
 const bot = new TelegramBot(token, { polling: true });
+const helpMsg = `Commands:\n/setlimit [amount] - Set weekly spending limit\n/startweek - Start a new week\n/spend [amount] - Log spending\n/getstatus - Get current status\n/help - Display this help message`;
+
 
 let weeklyLimit = 0;
 let startDate = null;
@@ -14,6 +16,7 @@ bot.on('message', (msg) => {
 
     if (messageText === '/start') {
         bot.sendMessage(chatId, 'Welcome to the spending tracker bot!');
+        bot.sendMessage(chatId, helpMsg);
     } else if (messageText.startsWith('/setlimit')) {
         weeklyLimit = parseFloat(messageText.split(' ')[1]);
         bot.sendMessage(chatId, `Weekly limit set to ${weeklyLimit} CZK.`);
@@ -37,7 +40,7 @@ bot.on('message', (msg) => {
         statusMsg += `Left: ${weeklyLimit - totalSpent} CZK`;
         bot.sendMessage(chatId, statusMsg);
     } else if (messageText === '/help') {
-        const helpMsg = `Commands:\n/setlimit [amount] - Set weekly spending limit\n/startweek - Start a new week\n/spend [amount] - Log spending\n/getstatus - Get current status\n/help - Display this help message`;
+
         bot.sendMessage(chatId, helpMsg);
     }
 });
